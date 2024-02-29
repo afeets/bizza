37251ae4ac7665ad89be13a042ab6a35cbb0900d
Andy Feetenby <andyfeetenby@gmail.com>
2024-02-29 08:32:48 +0700

    Inital Commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import SpeakerProfile from './SpeakerProfile';
+
+function App() {
+  return (
+    <div style={{ backgroundColor: 'gray', margin: 20, color: 'white'}}>
+      <SpeakerProfile />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/SpeakerProfile.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+/* 
+class SpeakerProfile extends React.Component {
+    render() {
+        return <h1>This is the class component expression from Speaker Profile</h1>;
+    }
+}
+*/
+
+/* function components accept props */
+const SpeakerProfile = () => {
+    return <h1>This is a function component, equivalent to class component</h1>
+}
+
+export default SpeakerProfile;
